c1bb514235022f13881143d06f12f8631ac6f4c6
Shimpei Takamatsu <shimpeiws@gmail.com>
2021-02-17 07:06:04 +0900

    Cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -3,22 +3,10 @@ import * as ReactDOM from "react-dom";
 import * as THREE from "three";
 import { ThreeComponent } from "./three";
 
-let rot = 0; // 角度
-let mouseX = 0; // マウス座標
-
-// マウス座標はマウスが動いた時のみ取得できる
-document.addEventListener("mousemove", (event) => {
-  mouseX = event.pageX;
-  const targetRot = (mouseX / window.innerWidth) * 360;
-  // イージングの公式を用いて滑らかにする
-  // 値 += (目標値 - 現在の値) * 減速値
-  rot += (targetRot - rot) * 0.02;
-});
-
 ReactDOM.render(
   <div>
     <h1>spike-three-js</h1>
-    <ThreeComponent mouseX={mouseX} rot={rot} />
+    <ThreeComponent />
   </div>,
   document.getElementById("app")
 );
